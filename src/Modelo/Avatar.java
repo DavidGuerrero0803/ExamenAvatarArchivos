de7cb4c33bd86effbf9e8239dd243aceaae229df
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-05-19 21:54:49 -0700

    Creada la clase Avatar, esta representa otra opción para crear un personaje, en caso de que no sea Guerrero o Maestro de un elemento.

---
+++ src/Modelo/Avatar.java
@@ -0,0 +1,34 @@
+package Modelo;
+
+public class Avatar extends Personaje implements IMaestroAgua, IMaestroTierra, IMaestroFuego, IMaestroAire {
+
+    public Avatar(String nombre, String nacion, String genero, int edad, boolean estaVivo, int nivelDeDominio, int energia) {
+        super(nombre, nacion, genero, edad, estaVivo, nivelDeDominio, energia);
+    }
+
+    @Override
+    public void controlarAgua() {
+        System.out.println(nombre + " ha dominado el Agua Control");
+    }
+
+    @Override
+    public void controlarTierra() {
+        System.out.println(nombre + " ha dominado la Tierra Control");
+    }
+
+    @Override
+    public void controlarFuego() {
+        System.out.println(nombre + " ha dominado el Fuego Control");
+    }
+
+    @Override
+    public void controlarAire() {
+        System.out.println(nombre + " ha dominado el Aire Control");
+    }
+
+    public void ataqueAvatar() throws EnergiaInsuficienteException {
+        verificarEnergiaSufiente();
+        System.out.println(nombre + " ha hecho un ataque con la fuerza de los 4 elementos");
+        energia -= 10;
+    }
+}
