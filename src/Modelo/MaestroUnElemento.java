602d96723c06e2033f07474f569ad1f0d9852c4d
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-05-19 20:19:39 -0700

    Creada la clase abstracta MaestroUnElemento, esta permitirá implementar su versión propia del método atacar() que se encuentra dentro de la clase.

---
+++ src/Modelo/MaestroUnElemento.java
@@ -0,0 +1,11 @@
+package Modelo;
+
+public abstract class MaestroUnElemento extends Personaje {
+
+    public MaestroUnElemento(String nombre, String nacion, String genero, int edad, boolean estaVivo, int nivelDeDominio, int energia) {
+        super(nombre, nacion, genero, edad, estaVivo, nivelDeDominio, energia);
+    }
+
+    abstract void atacar() throws EnergiaInsuficienteException;
+
+}
