62ed44c56f27dfcede946f83eecec3fd2224aa10
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-05-19 20:50:17 -0700

    La clase MaestroDelFuego funciona como clase concreta para heredar de MaestroUnElemento y hacer que 1 personaje controle dicho elemento.

---
+++ src/Modelo/MaestroDelFuego.java
@@ -0,0 +1,21 @@
+package Modelo;
+
+public class MaestroDelFuego extends MaestroUnElemento implements IMaestroFuego {
+
+    public MaestroDelFuego(String nombre, String nacion, String genero, int edad, boolean estaVivo, int nivelDeDominio, int energia) {
+        super(nombre, nacion, genero, edad, estaVivo, nivelDeDominio, energia);
+    }
+
+    @Override
+    public void controlarFuego() {
+        System.out.println(nombre + " se encuentra bajo Fuego Control");
+    }
+
+    @Override
+    public void atacar() throws EnergiaInsuficienteException {
+        verificarEnergiaSufiente();
+        // idea si descarto o no: que controlarFuego() salga como mensaje de preparación antes de atacar.
+        System.out.println(nombre + " realizó un ataque combinado de fuego y electricidad");
+        energia -= 10;
+    }
+}
