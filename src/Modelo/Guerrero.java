3e8f41c4f512e7f8ac6ae278a4cb57b97d0788d9
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-05-19 20:20:42 -0700

    Creada la clase Guerrero, esta extiende de personaje y permitirá identificar a aquellos personajes que no son parte de una maestría de un elemento.

---
+++ src/Modelo/Guerrero.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+public class Guerrero extends Personaje {
+
+    public Guerrero(String nombre, String nacion, String genero, int edad, boolean estaVivo, int nivelDeDominio, int energia) {
+        super(nombre, nacion, genero, edad, estaVivo, nivelDeDominio, energia);
+    }
+
+    public void realizarAtaque() throws EnergiaInsuficienteException {
+        verificarEnergiaSufiente();
+        System.out.println(nombre + " realizó un ataque con su arma (es muy efectivo)");
+        energia -= 10;
+    }
+}
