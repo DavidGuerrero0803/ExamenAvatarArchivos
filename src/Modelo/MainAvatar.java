4285089942cc5f73c0895ed97319c002475929b8
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-05-19 23:01:09 -0700

    Añadí el menú funcional con ayuda de un while, incluyendo las 3 opciones principales que quedan a implementar.

--- src/Modelo/MainAvatar.java
+++ src/Modelo/MainAvatar.java
@@ -14,7 +14,41 @@ public class MainAvatar {
         this.archivoCargado = false;
     }
 
-    public static void main(String[] args) {
+    public void ejecutarMenu() {
+        mostrarMenu();
+        scanner.close();
+    }
 
+    public void mostrarMenu() {
+        boolean menuActivo = true;
+        while (menuActivo) {
+            System.out.println("AVATAR | LA LEYENDA DEL ARCHIVO");
+            System.out.println("> 1 Crear Personaje");
+            System.out.println("> 2 Guardar y Salir");
+            System.out.println("> 3 Cargar Datos");
+            System.out.println("Escoge una opción: ");
+            int opcionElegida = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcionElegida) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    menuActivo = false;
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    System.out.println("Debes elegir entre una de las tres opciones.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MainAvatar ui = new MainAvatar();
+        ui.ejecutarMenu();
     }
 }
