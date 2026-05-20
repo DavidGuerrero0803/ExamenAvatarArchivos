1b9e6f6c9333eb635be57de9e8ad7c2974d93e4e
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-05-19 20:45:49 -0700

    La clase MaestroDeTierra funciona como clase concreta para heredar de MaestroUnElemento y hacer que 1 personaje controle dicho elemento.

---
+++ src/Modelo/MaestroDeTierra.java
@@ -0,0 +1,20 @@
+package Modelo;
+
+public class MaestroDeTierra extends MaestroUnElemento implements IMaestroTierra {
+
+    public MaestroDeTierra(String nombre, String nacion, String genero, int edad, boolean estaVivo, int nivelDeDominio, int energia) {
+        super(nombre, nacion, genero, edad, estaVivo, nivelDeDominio, energia);
+    }
+
+    @Override
+    public void controlarTierra() {
+        System.out.println(nombre + " se encuentra bajo Tierra Control");
+    }
+
+    @Override
+    public void atacar() throws EnergiaInsuficienteException {
+        verificarEnergiaSufiente();
+        System.out.println(nombre + " atacó con una gran piedra reforzada");
+        energia -= 10;
+    }
+}
