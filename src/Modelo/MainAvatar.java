4dd671fcd6bf7ef135f4c06f72eef789ee2a88fa
DavidGuerrero0803 <mario.guerrero.marquez@uabc.edu.mx>
2026-05-19 22:48:49 -0700

    La clase MainAvatar se le agregó sus atributos principales, un ArrayList, un objeto Scanner y un booleano para la carga de archivos.

--- src/Modelo/MainAvatar.java
+++ src/Modelo/MainAvatar.java
@@ -1,4 +1,20 @@
 package Modelo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainAvatar {
+    private ArrayList<Personaje> personajes;
+    private Scanner scanner;
+    private boolean archivoCargado;
+
+    public MainAvatar() {
+        this.personajes = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+        this.archivoCargado = false;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
